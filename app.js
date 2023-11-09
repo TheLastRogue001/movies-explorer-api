391e08328cbe3b9f79ebdafb1069d8a1e20bb9f5
Alex Shevelkov <sasha.shevelkov0@yandex.ru>
2023-11-10 00:18:18 +0300

    fix: refactoring code

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const routerMovies = require('./routes/movies');
 const routerAuth = require('./routes/auth');
 const auth = require('./middlewares/auth');
 const { NotFoundError } = require('./errors/errors');
+
 const { handleError } = require('./middlewares/handleError');
 
 const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/moviesdb' } = process.env;
