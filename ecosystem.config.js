5b9993aeecd0027fdee912db9196164691f79f47
Alex Shevelkov <sasha.shevelkov0@yandex.ru>
2023-11-10 00:10:42 +0300

    fix: refactoring ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'mesto-auto-deploy',
+      name: 'movies-auto-deploy',
       script: 'app.js',
     },
   ],
@@ -9,12 +9,12 @@ module.exports = {
   deploy: {
     production: {
       user: 'sasha0908',
-      host: '51.250.24.218',
-      ref: 'origin/main',
-      repo: 'git@github.com:TheLastRogue001/react-mesto-api-full-gha.git',
+      host: '51.250.22.46',
+      ref: 'origin/level-1',
+      repo: 'git@github.com:TheLastRogue001/movies-explorer-api.git',
       path: '/home/sasha0908/auto-deploy',
       'pre-deploy-local':
-        'scp .env sasha0908@51.250.24.218:/home/sasha0908/auto-deploy/current/backend',
+        'scp .env sasha0908@51.250.22.46:/home/sasha0908/auto-deploy/current/backend',
       'post-deploy':
         'pwd && cd backend && npm i && pm2 startOrRestart ecosystem.config.js --env production',
     },
