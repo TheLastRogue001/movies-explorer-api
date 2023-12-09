72ba72620950d23d517564c4e4e21086d788b870
Alex Shevelkov <sasha.shevelkov0@yandex.ru>
2023-12-09 16:26:39 +0300

    fix: refactoring code

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,12 +9,12 @@ module.exports = {
   deploy: {
     production: {
       user: 'sasha0908',
-      host: '51.250.22.46',
+      host: '84.201.167.76',
       ref: 'origin/level-1',
       repo: 'git@github.com:TheLastRogue001/movies-explorer-api.git',
       path: '/home/sasha0908/auto-deploy',
       'pre-deploy-local':
-        'scp .env sasha0908@51.250.22.46:/home/sasha0908/auto-deploy/current',
+        'scp .env sasha0908@84.201.167.76:/home/sasha0908/auto-deploy/current',
       'post-deploy':
         'pwd && npm i && pm2 startOrRestart ecosystem.config.js --env production',
     },
