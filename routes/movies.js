084198e43c3e4d596fbd256429392e3b35d5d4c2
Alex Shevelkov <sasha.shevelkov0@yandex.ru>
2023-12-11 14:30:01 +0300

    fix: refactoring code

--- routes/movies.js
+++ routes/movies.js
@@ -29,7 +29,7 @@ router.post(
         .required()
         .regex(urlRegex)
         .uri({ scheme: ['http', 'https'] }),
-      movieId: Joi.string().hex().length(24).required(),
+      movieId: Joi.number().hex().required(),
       nameEN: Joi.string().required().min(2).max(30),
       nameRU: Joi.string().required().min(2).max(30),
     }),
